0960091e8dfbf268731f167a1208f3458b2192d1
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2024-05-07 17:22:25 +0530

    add navbar

---
+++ src/components/layouts/landing.layouts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { LandingNavbar } from "../organisms/landingNavbar.component";
+
+function SaasLayout(props: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen flex flex-col ">
+      <LandingNavbar className="top-2" />
+      <div className="text-black dark:text-white flex-grow h-0 bg-gray-500">
+        Confidentail FHE Identity
+      </div>
+    </div>
+  );
+}
+
+export default SaasLayout;
