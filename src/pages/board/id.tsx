757144db885bc45406106b2719833d2645c41d5d
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2024-05-11 17:53:40 +0530

    create id

--- src/pages/board/id.tsx
+++ src/pages/board/id.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { EncryptedCard } from "@app/components/ui/encrypted-card";
+import React from "react";
+import { PiIdentificationBadgeFill } from "react-icons/pi";
 
 function id() {
   return (
-    <div>id</div>
-  )
+    <div className="p-2 h-full">
+      <h1 className="text-2xl text-gray-800  flex flex-row items-center gap-1 font-mono font-semibold p-2">
+        <PiIdentificationBadgeFill className="" /> <span>Identity</span>
+      </h1>
+      <div className="flex-grow overflow-y-scroll scrollbar-hide">
+        <div className="flex items-center justify-center p-8 w-full font-mono">
+          <div className="max-w-sm h-[30rem] border border-emerald-400 rounded-lg">
+            <EncryptedCard text="create_id" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default id
+export default id;
